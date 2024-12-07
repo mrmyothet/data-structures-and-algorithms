ed4a4f8e128fdc264a7abe3db87a0bd5acf14044
Myo Thet <mr.myothet@gmail.com>
2024-12-07 14:51:34 +0700

    rename test files into com.myothet.dsa.tests package

--- dsa/src/test/java/com/myothet/dsa/tests/TestCapital.java
+++ dsa/src/test/java/com/myothet/dsa/tests/TestCapital.java
@@ -1,9 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-
+package com.myothet.dsa.tests;
 
 import org.junit.jupiter.api.Assertions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
