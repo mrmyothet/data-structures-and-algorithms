ef32a4618395d14667774ac521d5b1031560e9e5
Myo Thet <mr.myothet@gmail.com>
2024-12-19 20:33:55 +0700

    delimiter matching - continue for negative cases

--- dsa/src/main/java/com/myothet/dsa/datastructure/Delimiter.java
+++ dsa/src/main/java/com/myothet/dsa/datastructure/Delimiter.java
@@ -1,30 +1,60 @@
 package com.myothet.dsa.datastructure;
 
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class Delimiter {
 
+    ArrayList<Character> startTags = new ArrayList<>();
+    ArrayList<Character> endTags = new ArrayList<>();
+
+    public Delimiter() {
+        startTags.add('[');
+        endTags.add('[');
+
+        startTags.add('{');
+        endTags.add('{');
+
+    }
+
     public boolean isValid(String input) {
-        
+
         Stack<Character> stack = new Stack<>();
-        
-        for (int i=0; i<input.length(); i++)
-        {
+
+        for (int i = 0; i < input.length(); i++) {
             char ch = input.charAt(i);
-            
-            if(ch=='[')
-            {
+
+            if (isStartTag(ch)) {
                 stack.push(ch);
-            }
-            
-            else if(ch == ']')
-            {
-                stack.pop();
+            } else if (isEndTag(ch)) {
+
+                if (stack.empty()) {
+                    return false;
+                }
+                if (isTagBalanced(stack, ch)) return false;
             }
         }
-        
-        
+
         return stack.isEmpty();
+    }
+
+    private boolean isEndTag(char ch) {
+        return endTags.contains(ch);
+    }
+
+    private boolean isStartTag(char ch) {
+        return startTags.contains(ch);
+    }
+
+    private boolean isTagBalanced(Stack<Character> stack, char ch) {
+        // top element must be start with balanced with ch
+        Character topElement = stack.pop();
+        int endTagsIndex = endTags.indexOf(ch);
+        int startTagIndex = startTags.indexOf(topElement);
+        if (endTagsIndex != startTagIndex) {
+            return true;
+        }
+        return false;
     }
 
 }
--- dsa/src/test/java/com/myothet/dsa/datastructure/DelimiterTest.java
+++ dsa/src/test/java/com/myothet/dsa/datastructure/DelimiterTest.java
@@ -1,12 +1,30 @@
 
 package com.myothet.dsa.datastructure;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class DelimiterTest {
     Delimiter delimiter = new Delimiter();
+    
+    @ParameterizedTest
+    @CsvSource(value = {
+        "'', true", 
+        "c[d], true", 
+        "a{bd}e,true",
+        "c[d, faluse", 
+        "a[b{c}d]e}, false", 
+        "a{b(c), false"
+    })
+    public void testWithParameters(String item, boolean expected)
+    {
+        boolean valid = delimiter.isValid(item);
+        assertEquals(valid, expected);
+    }
 
     @Test
     public void testEmpty()
@@ -41,5 +59,6 @@ public class DelimiterTest {
     public void testMultipleDelimiterNegative()
     {
         assertFalse(delimiter.isValid("a{bd}e"));
+        assertFalse(delimiter.isValid("a{b(c)"));
     }
 }
